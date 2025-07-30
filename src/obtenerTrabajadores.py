14b92f1833c22fb9a85934ca9c91e31882d249f7
Simon Valdez <simovaldez414@gmail.com>
2025-07-30 16:08:08 -0400

    SPVM: Test cases SVT001 modulo obtener trabajadores

---
+++ src/obtenerTrabajadores.py
@@ -0,0 +1,8 @@
+import requests
+
+def obtenerTrabajadores(getUrl):
+    endpoint = "obtenerTrabajadores/Modulo 4"
+    lista_url = getUrl + endpoint
+    response = requests.get(lista_url)
+    listaTrabajadores = response.json()
+    return listaTrabajadores
