5f01298132f127e65823ce1bb20d4ec6d4afe669
RonnPC <201801673@est.umss.edu>
2025-08-01 17:56:00 -0400

    Cambios testCases

--- tests/api_infinitychess/listaTutoresPorSede/RPL008.py
+++ tests/api_infinitychess/listaTutoresPorSede/RPL008.py
@@ -2,7 +2,7 @@ import requests
 import pytest
 
 @pytest.mark.smoke
-def test_RPL001_obtenerTutorPorSedeValida (getUrl):
+def test_RPL008_obtenerTutorPorSedeValida (getUrl):
     endpoint = "obtenerTutores/Modulo 4"
     lista_url = getUrl + endpoint
     response = requests.get(lista_url)
