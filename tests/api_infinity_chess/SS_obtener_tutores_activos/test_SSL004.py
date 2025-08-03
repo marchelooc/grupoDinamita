42cdac4576404f2d7715ca56d278c23ef092381c
Sergio <201801656@est.umss.edu>
2025-08-03 15:56:26 -0400

    SBSN: Implementacion de loggers en test cases SSL001,SSL002,SSL003,SSL004,SSL005,SSL006,SSL007,SSL008,SSL009,SSL010,SSL011,SSL012,SSL013,SSL014,SSL015.

--- tests/api_infinity_chess/SS_obtener_tutores_activos/test_SSL004.py
+++ tests/api_infinity_chess/SS_obtener_tutores_activos/test_SSL004.py
@@ -2,11 +2,17 @@ import requests
 import pytest
 from src.assertions.add import assert_validar_response_schema
 from src.utils.cargar_schema import cargar_schema
+from src.utils.logger_config import logger
 
 @pytest.mark.smoke
 def test_verificación_del_código_de_respuesta(get_url):
+    logger.info("Iniciando test SSL004.")
     endpoint = "obtenerTutoresActivos"
     lista_url = get_url + endpoint
+    logger.info(f"Enviando GET a {lista_url}.")
     response = requests.get(lista_url)
+    logger.info(f"Código de respuesta: {response.status_code}.")
     assert response.status_code == 200
+    logger.info("Validando schema del response.")
     assert_validar_response_schema(response,cargar_schema("schema_lista_tutores.json"))
+    logger.info("Test completado.")
