283a5db7a5569d0b9b94a9c9c98a685128f55841
miguelangel8888 <mkalyu81@gmail.com>
2025-08-04 18:26:09 -0400

    Modificando archivo obtener curso

--- src/api_infinity_chess/obtener_curso.py
+++ src/api_infinity_chess/obtener_curso.py
@@ -1,23 +1,23 @@
 import requests
 import random
-def obtener_cursos(getUrl):
+def obtener_cursos(get_url):
     endpoint = "obtenerCursos/Modulo 4"
-    lista_url = getUrl + endpoint
+    lista_url = get_url + endpoint
     response = requests.get(lista_url)
     listaCursos = response.json()
     return listaCursos
 
 
-def obtener_cod_materia(getUrl):
-    cursos=obtener_cursos(getUrl)
+def obtener_cod_materia(get_url):
+    cursos=obtener_cursos(get_url)
     CODMATERIA = random.choice(cursos)["CODCURSO"]
     return CODMATERIA
 
 
 
-def obtener_nombre_grupo_existente(getUrl, CODCURSO):
+def obtener_nombre_grupo_existente(get_url, CODCURSO):
     endpoint = f"obtenerGrupo/{CODCURSO}/Modulo 4"
-    response = requests.get(getUrl + endpoint)
+    response = requests.get(get_url + endpoint)
     response.raise_for_status()
     grupos = response.json()
 
