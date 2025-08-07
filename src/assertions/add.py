dddb2bbd19a10e5503481fe0cb05e1a644b16b00
Sergio <201801656@est.umss.edu>
2025-08-07 10:41:27 -0400

    DEV: Modificacion de logger

--- src/assertions/add.py
+++ src/assertions/add.py
@@ -6,17 +6,17 @@ from src.utils.logger_config import logger
 def assert_validar_response_schema(response, schema):
     try:
         jsonschema.validate(instance=response.json(), schema=schema)
-        logger.debug("Schema validado correctamente.")
+        logger.debug("Response validado correctamente con el Schema.")
         return True
     except jsonschema.exceptions.ValidationError as err:
-        logger.debug(f"El JSON no coincide con el esquema: {err.message}")
-        pytest.fail(f"El JSON no coincide con el esquema: {err.message}")
+        logger.debug(f"El Response no coincide con el Schema: {err.message}")
+        pytest.fail(f"El Response no coincide con el Schema: {err.message}")
 
 def assert_validar_schema_input(data, schema,contexto=""):
     try:
         jsonschema.validate(instance=data, schema=schema)
-        logger.debug("Schema validado correctamente.")
+        logger.debug("Payload validado correctamente con el Schema.")
         return True
     except jsonschema.exceptions.ValidationError as err:
-        logger.debug(f"El JSON no coincide con el esquema: {err.message}")
-        pytest.fail(f"El JSON no coincide con el esquema: {err.message}")
+        logger.debug(f"El Payload no coincide con el Schema: {err.message}")
+        pytest.fail(f"El Payload no coincide con el Schema: {err.message}")
